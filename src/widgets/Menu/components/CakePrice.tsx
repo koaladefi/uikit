22623bb020fb1d42d25bf178af9e61b63f9c33aa
baby-koala <monkeyfinance42@protonmail.com>
2021-03-04 19:53:07 +0100

    fix link

--- src/widgets/Menu/components/CakePrice.tsx
+++ src/widgets/Menu/components/CakePrice.tsx
@@ -23,7 +23,7 @@ const PriceLink = styled.a`
 
 const CakePrice: React.FC<Props> = ({ cakePriceUsd }) => {
   return cakePriceUsd ? (
-    <PriceLink href="https://pancakeswap.info/token/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82" target="_blank">
+    <PriceLink href="https://pancakeswap.info/token/0xba26397cdff25f0d26e815d218ef3c77609ae7f1" target="_blank">
       <PancakeRoundIcon width="24px" mr="8px" />
       <Text color="textSubtle" bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
     </PriceLink>
