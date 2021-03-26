debbff9d2543498733f6ae38c7bd61581c85f936
baby-koala <monkeyfinance42@protonmail.com>
2021-03-26 17:00:58 +0100

    update price link

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2564,7 +2564,7 @@ var templateObject_1$D;
 var PriceLink = styled.a(templateObject_1$E || (templateObject_1$E = __makeTemplateObject(["\n  display: flex;\n  align-items: center;\n  svg {\n    transition: transform 0.3s;\n  }\n  :hover {\n    svg {\n      transform: scale(1.2);\n    }\n  }\n"], ["\n  display: flex;\n  align-items: center;\n  svg {\n    transition: transform 0.3s;\n  }\n  :hover {\n    svg {\n      transform: scale(1.2);\n    }\n  }\n"])));
 var CakePrice = function (_a) {
     var cakePriceUsd = _a.cakePriceUsd;
-    return cakePriceUsd ? (React.createElement(PriceLink, { href: "https://pancakeswap.info/token/0xba26397cdff25f0d26e815d218ef3c77609ae7f1", target: "_blank" },
+    return cakePriceUsd ? (React.createElement(PriceLink, { href: "https://dex.guru/token/0xba26397cdff25f0d26e815d218ef3c77609ae7f1-bsc", target: "_blank" },
         React.createElement(Icon$x, { width: "24px", mr: "8px" }),
         React.createElement(Text, { color: "textSubtle", bold: true }, "$" + cakePriceUsd.toFixed(3)))) : (React.createElement(Skeleton, { width: 80, height: 24 }));
 };
